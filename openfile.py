a4da14dad21cc19f6a3d87624cd5060cb7752976
michael chen <michaelchen@michaeldeMacBook-Pro.local>
2021-07-05 21:56:09 +0800

    Initial commit

---
+++ openfile.py
@@ -0,0 +1,10 @@
+# fin =open('/Users/michaelchen/Desktop/mictest.txt',r,encoding='utf-8')
+# s=fin.read()
+# print(s)
+# fin.close()
+
+fin = open('/Users/michaelchen/Desktop/Test.txt', 'rw')
+s = fin.readline()
+print(s)
+fin.close()
+
